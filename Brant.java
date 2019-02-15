4cfd69038c4fd37c487f72bdf5ca2620568e6a43
Saige Liu <saige.liu@madoor.com>
2019-02-15 15:31:42 -0800

    Scanner

--- Brant.java
+++ Brant.java
@@ -1,50 +1,59 @@
+import java.util.Scanner;
+
 public class Brant{
     public static void main(String[] args) {
-        if(args.length>0){
-            int number= Integer.parseInt(args[0]);
-            int[] intArray =new int[number*(number-1)/2];
-            int count=0;
-            try {
-                for(int i=0; i<number-1;i++){
-                    
-                    for (int j=i+1; j< number;j++){
-                        
-                        int temp=Integer.parseInt(args[i+1])+Integer.parseInt(args[j+1]);
-                    
-                        intArray[count]=temp;
-                        count++;
-                    }
-                }
-                
-            } catch (Exception e) {
-                System.err.println("Invalid Input");
-            }
-            
+        Scanner reader= new Scanner(System.in);
+        System.out.println("Enter your user input ");
+
         
-            int maxnumber=0;
-            int maxpair=0;
-        
-            
-            for (int k=0;k<intArray.length; k++) {
-                
-                int temp=1;
-                for(int g=k+1;g<intArray.length;g++){
-                    if(intArray[k]==intArray[g]){
-                        temp++;
-                    }
-                }
-                if (temp>maxnumber){
-                    maxnumber=temp;
-                    maxpair=1;
-                
-                }
-                else if (temp==maxnumber){
-                    maxpair++;
-                }
-                
-            }
-            System.out.println(maxnumber+"\n"+maxpair);
+        int number= reader.nextInt();
+        int[] intInput =new int[number];
+        for(int u=0; u<number;u++){
+            intInput[u]=reader.nextInt();
         }
+        int[] intArray =new int[number*(number-1)/2];
+        int count=0;
+        try {
+            for(int i=0; i<number;i++){
+                
+                for (int j=i+1; j< number;j++){
+                    //System.out.println(intInput[i]+"\n"+intInput[j]);
+                    int temp=intInput[i]+intInput[j];
+                
+                    intArray[count]=temp;
+                    count++;
+                }
+            }
+            
+        } catch (Exception e) {
+            System.err.println("Invalid Input");
+        }
+        
+    
+        int maxnumber=0;
+        int maxpair=0;
+    
+        
+        for (int k=0;k<intArray.length; k++) {
+            
+            int temp=1;
+            for(int g=k+1;g<intArray.length;g++){
+                if(intArray[k]==intArray[g]){
+                    temp++;
+                }
+            }
+            if (temp>maxnumber){
+                maxnumber=temp;
+                maxpair=1;
+            
+            }
+            else if (temp==maxnumber){
+                maxpair++;
+            }
+            
+        }
+        System.out.println(maxnumber+"\n"+maxpair);
+        
     }
 }
 
